271d97996eca2d7acf952443291922dfe42e0e34
Rabbi1089 <rabbi.ext@gmail.com>
2025-02-13 23:29:09 +0600

    add user role

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run() {
     User related api
     */
 
-    app.patch("/users/admin/id", async (req, res) => {
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
