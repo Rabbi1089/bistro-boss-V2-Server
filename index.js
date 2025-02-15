cf161fceb52be3868aabe56d69ae0a532b00168b
Rabbi1089 <rabbi.ext@gmail.com>
2025-02-15 23:24:24 +0600

    add jwt token and verify token

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 //without require dotenv env will not work
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-//middlware
-
 app.use(cors());
 app.use(express.json());
+
+    //middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({
+          message: "Unauthorized access",
+        });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(403).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        console.log('from decoded', decoded);
+      });
+      next();
+    };
+
+    //verify admin , use verify admin after verify token
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      console.log('from verify admin middleware' , email);
+      const query = { email: email};
+      const user = await userCollections.findOne(query);
+      const isAdmin = user?.role === 'admin'
+      if (isAdmin) {
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next()
+    }
+
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.t241ufd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -30,9 +62,40 @@ async function run() {
     const cartCollections = client.db("bisrtoDB").collection("carts");
     const userCollections = client.db("bisrtoDB").collection("user");
     /*
+    Json web token related api
+    */
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+       expiresIn: '1h' ,
+      });
+      res.send({ token });
+    });
+
+
+    /*
     admin related api
     */
-    app.get("/user", async (req, res) => {
+   app.get('/users/admin/:email', verifyToken , async(req,res) => {
+    const email = req.params.email;
+    console.log(req.decoded.email);
+    if (email !== req.decoded.email) {
+      return res.status(403).send({message : 'unauthorized access'})
+    }
+    const query = { email: email}
+    const user = await userCollections.findOne(query)
+    let admin = false;
+    if (user) {
+      admin = user?.role === 'admin'
+    }
+    res.send({admin})
+   })
+
+
+
+
+
+    app.get("/user", verifyToken, async (req, res) => {
       const result = await userCollections.find().toArray();
       res.send(result);
     });
