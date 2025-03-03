eb08357bfbaddd55406fb652d666de0c8671f415
Rabbi1089 <rabbi.ext@gmail.com>
2025-03-03 13:04:53 +0600

    add api to delete and update a item

--- index.js
+++ index.js
@@ -9,38 +9,24 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-    //middlewares
-    const verifyToken = (req, res, next) => {
-      if (!req.headers.authorization) {
-        return res.status(401).send({
-          message: "Unauthorized access",
-        });
-      }
-      const token = req.headers.authorization.split(" ")[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-          return res.status(403).send({ message: "forbidden access" });
-        }
-        req.decoded = decoded;
-        console.log('from decoded', decoded);
-      });
-      next();
-    };
+//middlewares --------------
 
-    //verify admin , use verify admin after verify token
-
-    const verifyAdmin = async (req, res, next) => {
-      const email = req.decoded.email;
-      console.log('from verify admin middleware' , email);
-      const query = { email: email};
-      const user = await userCollections.findOne(query);
-      const isAdmin = user?.role === 'admin'
-      if (isAdmin) {
-        return res.status(403).send({message: 'forbidden access'})
-      }
-      next()
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({
+      message: "Unauthorized access",
+    });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
     }
-
+    req.decoded = decoded;
+    console.log("from decoded", decoded);
+  });
+  next();
+};
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.t241ufd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -67,35 +53,45 @@ async function run() {
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-       expiresIn: '1h' ,
+        expiresIn: "1h",
       });
       res.send({ token });
     });
 
+    //verify admin , use verify admin after verify token
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      console.log("from verify admin middleware", email);
+      const query = { email: email };
+      const user = await userCollections.findOne(query);
+      const isAdmin = user?.role === "admin";
+      console.log(isAdmin);
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
 
     /*
     admin related api
     */
-   app.get('/users/admin/:email', verifyToken , async(req,res) => {
-    const email = req.params.email;
-    console.log(req.decoded.email);
-    if (email !== req.decoded.email) {
-      return res.status(403).send({message : 'unauthorized access'})
-    }
-    const query = { email: email}
-    const user = await userCollections.findOne(query)
-    let admin = false;
-    if (user) {
-      admin = user?.role === 'admin'
-    }
-    res.send({admin})
-   })
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      console.log(req.decoded.email);
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      const query = { email: email };
+      const user = await userCollections.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
 
-
-
-
-
-    app.get("/user", verifyToken, async (req, res) => {
+    app.get("/user", verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollections.find().toArray();
       res.send(result);
     });
@@ -104,19 +100,24 @@ async function run() {
     User related api
     */
 
-    app.patch("/users/admin/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updatedDoc = {
-        $set: {
-          role: "admin",
-        },
-      };
-      const result = await userCollections.updateOne(filter, updatedDoc);
-      res.send(result);
-    });
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await userCollections.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
 
-    app.delete("/user/:id", async (req, res) => {
+    app.delete("/user/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const query = { _id: new ObjectId(id) };
@@ -138,10 +139,59 @@ async function run() {
       res.send(result);
     });
 
-    //----------- Menu related api
+    //----------- Menu related api -------------------
     app.get("/menu", async (req, res) => {
       const result = await menuCollections.find().toArray();
       res.send(result);
+    });
+
+    app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollections.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollections.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      // Prepare query to match both formats
+      const query = ObjectId.isValid(id)
+        ? { $or: [{ _id: new ObjectId(id) }, { _id: id }] }
+        : { _id: id };
+      // console.log("MongoDB Query:", query);
+      // const query = { _id: new ObjectId(id) };
+      const result = await menuCollections.findOne(query);
+      res.send(result);
+    });
+    //If IDs appear as strings, do not use new ObjectId(id).
+
+    app.patch("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const item = req.body;
+      console.log(id, item);
+      // Prepare query to match both formats
+      const query = ObjectId.isValid(id)
+        ? { $or: [{ _id: new ObjectId(id) }, { _id: id }] }
+        : { _id: id };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          recipe: item.recipe,
+          image: item.display_url,
+          category: item.category,
+          price: item.price,
+        },
+      };
+
+      const result = await menuCollections.updateOne(query, updatedDoc);
+      res.send(result);
+      console.log(id, item, result);
     });
 
     //---------- Review related api
